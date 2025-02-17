10513176cdada0973910f8431ad21f50b9af5fb2
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-02-17 13:37:42 +0100

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,6 @@ import { Toaster, toast } from 'sonner';
 import Image from 'next/image';
 import '../globals.css';
 
-const PORTAL_BASE_URL = process.env.NEXT_PUBLIC_PORTAL_BASE_URL;
 
 export default function Login() {
   const [username, setUsername] = useState<string>('');
@@ -46,7 +45,7 @@ export default function Login() {
             <div className=" block sm:mx-auto sm:w-full sm:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-white-800 dark:border-gray-200 mx-auto ">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <Image
-                        src={`${PORTAL_BASE_URL}/bixdata/logos/telamico.png`}
+                        src="/bixdata/logos/telamico.png"
                         alt="BixData"
                         width={1000}
                         height={1000}
