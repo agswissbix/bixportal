3535f65d15006c3f53321588d43430b9c176fc45
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-02-17 16:08:17 +0100

    .

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,26 +1,16 @@
 // utils/axiosInstance.ts
 import axios from 'axios';
-import https from 'https';
 import Cookies from 'js-cookie';
 import Router from 'next/router';
 
-// Controlla se siamo in ambiente di sviluppo
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-// Se in sviluppo, crea un agent che disabilita la verifica del certificato
-let httpsAgent;
-if (isDevelopment) {
-  httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-  });
-}
-
 // Crea un'istanza axios preconfigurata
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, 
-  withCredentials: true, // Invia automaticamente i cookie di sessione con ogni richiesta
-  // Usa l'httpsAgent in development
-  ...(isDevelopment && { httpsAgent }),
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Base URL del backend Django
+    withCredentials: true, // Invia automaticamente i cookie di sessione con ogni richiesta
 });
+
+
+
+
 
 export default axiosInstance;
