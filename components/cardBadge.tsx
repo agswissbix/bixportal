80cc54c268c01a7e54f452e572d951a62fc37ccb
marcogarganigo <marcogarga11@gmail.com>
2025-04-08 12:20:25 +0200

    -

--- components/cardBadge.tsx
+++ components/cardBadge.tsx
@@ -2,6 +2,7 @@ import React, { useMemo, useContext, useState, useEffect } from 'react';
 import { useApi } from '@/utils/useApi';
 import GenericComponent from './genericComponent';
 import { AppContext } from '@/context/appContext';
+import ImagePreview from './imagePreview';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 // FLAG PER LO SVILUPPO
@@ -12,6 +13,7 @@ const isDev = false;
         interface PropsInterface {
           tableid?: string;
           recordid?: string;
+          type?: string;
         }
 
         // INTERFACCIA RISPOSTA DAL BACKEND
@@ -19,6 +21,7 @@ const isDev = false;
             badgeItems: Array<{
                 fieldid: string;
                 value: string;
+                type?: string;
             }>
         }
 
@@ -37,27 +40,33 @@ export default function ExampleComponentWithData({ tableid,recordid }: PropsInte
                 badgeItems: [
                     {
                         fieldid: "test1",
-                        value: "test1"
+                        value: "test1",
+                        type: "text"
                     },
                     {
                         fieldid: "test2",
-                        value: "test2"
+                        value: "test2",
+                        type: "text"
                     },
                     {
                         fieldid: "test3",
-                        value: "test3"
+                        value: "test3",
+                        type: "text"
                     },
                     {
                         fieldid: "test4",
-                        value: "test4"
+                        value: "test4",
+                        type: "text"
                     },
                     {
                         fieldid: "test5",
-                        value: "test5"
+                        value: "test5",
+                        type: "text"
                     },
                     {
-                        fieldid: "test6",
-                        value: "test6"
+                        fieldid: "test7",
+                        value: "projecttemplatemilestone/00000000000000000000000000000003",
+                        type: "attachment"
                     }
                 ]
             };
@@ -81,6 +90,7 @@ export default function ExampleComponentWithData({ tableid,recordid }: PropsInte
 
     // CHIAMATA AL BACKEND (solo se non in sviluppo) (non toccare)
     const { response, loading, error } = !isDev && payload ? useApi<ResponseInterface>(payload) : { response: null, loading: false, error: null };
+    
 
     // AGGIORNAMENTO RESPONSE CON I DATI DEL BACKEND (solo se non in sviluppo) (non toccare)
     useEffect(() => {
@@ -94,11 +104,20 @@ export default function ExampleComponentWithData({ tableid,recordid }: PropsInte
             {(response: ResponseInterface) => (
                 <div className="h-full w-full flex justify-center items-center">
                     <div className="flex flex-wrap justify-center w-full h-5/6 bg-secondary rounded-xl p-3">
-                    <img src="http://localhost:8000/media/fotostabile.jpg" alt="Allegato" />
                         {response.badgeItems.map((item) => (
+                           item.fieldid === "fotostabile" ? (
+                            //check if item.value is not null, i'm in a react tsx template
+                            item.value ? (
+                            <ImagePreview 
+                                    key={item.fieldid}
+                                    imageUrl={`http://localhost:8000/commonapp/media/${item.value}`}
+                                />
+                            ) : null
+                           ) : (
                             <p key={item.fieldid} className="w-1/3 text-center text-white">
-                                {item.value}
-                            </p>
+                                    {item.value}
+                                </p>
+                           )
                         ))}
                     </div>
                 </div>
---
+++ components/imagePreview.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const ImagePreview = ({ imageUrl }: { imageUrl: string }) => {
+  const [showFull, setShowFull] = useState(false);
+
+  return (
+    <div>
+      {/* Preview thumbnail */}
+      <img
+        src={imageUrl}
+        alt="allegato"
+        onClick={() => setShowFull(true)}
+        style={{
+          width: '5%',
+          height: 'auto',
+          cursor: 'pointer',
+          borderRadius: '8px',
+          boxShadow: '0 2px 6px rgba(0,0,0,0.2)',
+        }}
+      />
+
+      {/* Modal full screen */}
+      {showFull && (
+        <div
+          onClick={() => setShowFull(false)}
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: 'screen',
+            height: 'screen',
+            backgroundColor: 'rgba(0, 0, 0, 0.3)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 9999,
+            cursor: 'zoom-out',
+            borderRadius: '10px',
+            padding: '10px',
+          }}
+        >
+          <img
+            src={imageUrl}
+            alt="allegato full"
+            style={{ maxWidth: '90%', maxHeight: '90%', borderRadius: '10px' }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImagePreview;
