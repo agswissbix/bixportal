d3b965d471db1519b1f435b477c4c2865fb5f1d7
agswissbix <108857221+agswissbix@users.noreply.github.com>
2025-04-11 13:29:01 +0200

    .

--- components/cardTabs.tsx
+++ components/cardTabs.tsx
@@ -8,9 +8,11 @@ import GenericComponent from './genericComponent';
 interface PropsInterface {
   tableid: string;
   recordid: string;
+  mastertableid?: string;
+  masterrecordid?: string;
 }
 
-export default function ExampleComponent({ tableid,recordid }: PropsInterface) {
+export default function ExampleComponent({ tableid,recordid,mastertableid,masterrecordid }: PropsInterface) {
 
   const {addCard} = useRecordsStore();
   const [activeTab, setActiveTab] = useState('Fields')
@@ -86,7 +88,7 @@ export default function ExampleComponent({ tableid,recordid }: PropsInterface) {
 
           <div className="h-5/6 p-4">
             {activeTab === 'Fields' ? (
-              <CardFields tableid={tableid} recordid={recordid} />
+              <CardFields tableid={tableid} recordid={recordid} mastertableid={mastertableid} masterrecordid={masterrecordid} />
             ) : (
               <CardLinked tableid={tableid} recordid={recordid} />
             )}
