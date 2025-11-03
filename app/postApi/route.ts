a81bbb50d155c1d62ca625f29a76634864ef057f
Sara Bressan <sara.bressan@swissbix.ch>
2025-11-03 10:41:17 +0100

    Update corrected route for calendar sync

--- app/postApi/route.ts
+++ app/postApi/route.ts
@@ -121,7 +121,7 @@ export async function POST(request: Request) {
     case 'get_calendar_records': djangoUrl = '/commonapp/get_calendar_records/'; break;
     case 'get_records_matrixcalendar': djangoUrl = '/commonapp/get_records_matrixcalendar/'; break;
     case 'matrixcalendar_save_record': djangoUrl = '/commonapp/matrixcalendar_save_record/'; break;
-    case 'sync_graph_calendar': djangoUrl = "/commonapp_swissbix/sync_graph_calendar/"; break;
+    case 'sync_graph_calendar': djangoUrl = "/customapp_swissbix/sync_graph_calendar/"; break;
     case 'set_column_order': djangoUrl = '/commonapp/set_column_order/'; break;
     case 'getPitservicePivotLavanderie': djangoUrl = '/commonapp/get_pitservice_pivot_lavanderia/'; break;
     case 'save_record_fields': djangoUrl = '/commonapp/save_record_fields/'; break;
