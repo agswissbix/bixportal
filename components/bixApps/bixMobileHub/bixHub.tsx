1fda244b358b02cbee6958b3f156001eac8a2059
riccardodeiuri <riccardo.deiuri@swissbix.ch>
2026-04-02 16:57:57 +0200

    fix link bixhub _blank

--- components/bixApps/bixMobileHub/bixHub.tsx
+++ components/bixApps/bixMobileHub/bixHub.tsx
@@ -298,6 +298,7 @@ export default function BixHub() {
                                                  const theme = getTheme(index);
                                                  return (
                                                     <Link 
+                                                        target="_blank"
                                                         href={app.url} 
                                                         key={index}
                                                         className={`
